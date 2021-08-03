aa8af32210c5f5cd8222101cb274193e198ebf72
apatel121386 <apatel121386@outlook.com>
2021-08-03 17:05:20 -0500

    day2form

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import render_template
+from app.forms import RegisterForm
+
+@app.route('/')
+def index():
+    name = 'Ankit'
+    title = 'Coding Temple Intro to Flask'
+    return render_template('index.html', name=name, title=title)
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        print('THE FORM IS VALID!!!!')
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(username, email, password)
+        
+    return render_template('register.html', title='Register for CT Blog', form=form)
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config:
+    SECRET_KEY = 'you-will-never-know-this-secret-key'
