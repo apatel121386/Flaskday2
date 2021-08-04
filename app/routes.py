fd8a09f9cba1bf1f85de327aedf5d5633b92fd3d
apatel121386 <apatel121386@outlook.com>
2021-08-04 15:05:09 -0500

    Wednesday commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from app import app
+from app import app, db
 from flask import render_template
 from app.forms import RegisterForm
+from app.models import User
+
 
 @app.route('/')
 def index():
@@ -12,10 +14,17 @@ def index():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        #Grab data from the submitted form
         print('THE FORM IS VALID!!!!')
         username = form.username.data
         email = form.email.data
         password = form.password.data
         print(username, email, password)
+        # Create new instance of User
+        new__user = User(username, email, password)
+        
+        #Add new__user to the database
+        db.session.add(new__user)
+        db.session.commit()
         
     return render_template('register.html', title='Register for CT Blog', form=form)
--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
 
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Windows- C:\Users\Ankit\Documents\CodingTemple-June-2021\Week6\day2.2\intro_to_flask_jun
+
 class Config:
     SECRET_KEY = 'you-will-never-know-this-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
