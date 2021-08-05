723732f3921f71de462e699d9d0957323ca8e14d
apatel121386 <apatel121386@outlook.com>
2021-08-05 11:55:52 -0500

    wednesday final

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,19 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for
-from app.forms import RegisterForm
-from app.models import User
-from app.models import Post
-
-
+from app.forms import RegisterForm, CreatePostForm
+from app.models import User, Post
 
 @app.route('/')
 def index():
     name = 'Ankit'
-    title = 'Coding Temple Intro to Flask'
+    title = "Ankit's Blog"
     return render_template('index.html', name=name, title=title)
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        #Grab data from the submitted form
-        print('THE FORM IS VALID!!!!')
+        # Grab data from our submitted form
         username = form.username.data
         email = form.email.data
         password = form.password.data
@@ -25,14 +21,42 @@ def register():
         # Create new instance of User
         new_user = User(username, email, password)
         
-        #Add new__user to the database
+
+    
+
+        # Add new_user to our database
         db.session.add(new_user)
         db.session.commit()
+
+        # Once new_user is added to db, flash success message
+        flash(f'Thank you for signing up {new_user.username}!', 'info')
+
+        # Redirect user back to home page
+        return redirect(url_for('index'))
+    
+           
+    return render_template('register.html', title='Register for CT Blog', form=form)
+
+
+@app.route('/createpost', methods= ['GET', 'POST'])
+def createpost():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        #Grab data from form
+        title = form.title.data
+        body = form.body.data
+        print(title,body)
+        #Create new instance of Post from form database
+        new_post = Post(title, body, 1)
         
-        # Once new_user is added to db, flash success messages
-        flash(f'Thank you for registering {new_user.username}!', 'primary')
+        #Add new_post to database
+        db.session.add(new_post)
+        db.session.commit()
         
-        #Redirect user back to home page
+        #Flash Message for post 
+        flash(f'Your post has been created successfully {new_post.author.username}!', 'success')
+        
+        # Redirect user back to home page
         return redirect(url_for('index'))
         
-    return render_template('register.html', title='Register for CT Blog', form=form)
+    return render_template('createpost.html', form=form)
